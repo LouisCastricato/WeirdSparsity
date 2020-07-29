afa245dbe72bd72bb3002388f1dc53f854255d94
Louis Castricato <wirezapp@gmail.com>
2020-07-29 19:25:45 -0400

    aaaa

--- boringtest.py
+++ boringtest.py
@@ -19,7 +19,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 input_size = 784
 hidden_size = 500
 num_classes = 10
-num_epochs = 10
+num_epochs = 50
 batch_size = 100
 learning_rate = 0.0001
 
