babb16baf3a707dcd0880fef88409964910d454a
Louis Castricato <wirezapp@gmail.com>
2020-07-29 20:29:51 -0400

    testing conv

--- boringtest.py
+++ boringtest.py
@@ -93,7 +93,32 @@ class NeuralNet(nn.Module):
         
         return self.fc5(out)
 
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = torch.nn.Conv2d(3, 6, 5)
+        self.pool = torch.nn.MaxPool2d(2, 2)
+        self.conv2 = torch.nn.Conv2d(6, 16, 5)
+        self.fc1 = torch.nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = torch.nn.Linear(120, 84)
+        self.fc3 = torch.nn.Linear(84, 10)
+
+        self.tks1 = TopK_custom(110, max_iter=50)
+        self.tks2 = TopK_custom(75, max_iter=50)
+
+    def forward(self, x):
+        x = self.pool(torch.F.relu(self.conv1(x)))
+        x = self.pool(torch.F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = self.tks1(torch.F.relu(self.fc1(x)))
+        x = self.tks2(torch.F.relu(self.fc2(x)))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
 model = NeuralNet(input_size, hidden_size, num_classes).to(device)
+model = Net().to(device)
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
