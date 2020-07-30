f59fcd1d6f7cb09252cd39825403b9f2e4badbb4
Louis Castricato <wirezapp@gmail.com>
2020-07-29 21:13:36 -0400

    LR change

--- boringtest.py
+++ boringtest.py
@@ -23,7 +23,7 @@ hidden_size = 500
 num_classes = 10
 num_epochs = 20
 batch_size = 100
-learning_rate = 0.001
+learning_rate = 0.1
 
 # MNIST dataset 
 train_dataset = torchvision.datasets.CIFAR10(root='../../data', 
